e244c915d948fa1252d3c03552a76d4a4a0e7620
Jakub Czarnota <kuba.czarnota05@outlook.com>
2024-05-02 19:15:16 +0200

    Add config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import config from './config.js'
 
 const app = express()
 
-
+app.listen(config.server.port, () => {
+    console.log(`Listening on port ${config.server.port}`);
+})
